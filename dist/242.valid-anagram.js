c6961caed736bfcaf1c78624fa3daccd3c4a48b6
AdamAllanThomas <adamathomas100@gmail.com>
2023-06-03 15:14:25 -0600

    anagram

---
+++ dist/242.valid-anagram.js
@@ -0,0 +1,10 @@
+"use strict";
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+function isAnagram(s, t) {
+    // O(n log n)
+    if (s.split("").sort().toString() === t.split("").sort().toString())
+        return true;
+    else
+        return false;
+}
