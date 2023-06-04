a8974619fe2441d23f7503c3902ddcaf51e59f4e
AdamAllanThomas <adamathomas100@gmail.com>
2023-06-04 15:30:07 -0600

    383

---
+++ src/383.ransom-note.ts
@@ -0,0 +1,30 @@
+// Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+// Each letter in magazine can only be used once in ransomNote.
+
+function canConstruct(ransomNote: string, magazine: string): boolean {
+  let letters = new Map();
+  for (let letter of magazine) {
+    if (!letters.has(letter)) letters.set(letter, 1);
+    else letters.set(letter, letters.get(letter) + 1);
+  }
+  for (let letter of ransomNote) {
+    if (!letters.has(letter) || letters.get(letter) === 0) return false;
+    else letters.set(letter, letters.get(letter) - 1);
+  }
+  return true;
+}
+
+function realBadCanConstruct(ransomNote: string, magazine: string): boolean {
+  // O(n^2)
+  const note = [...ransomNote],
+    mag = [...magazine];
+  for (let i = 0; i < note.length; i++) {
+    if (mag.indexOf(note[i]) === -1) {
+      return false;
+    } else {
+      delete mag[i];
+    }
+  }
+  return true;
+}
