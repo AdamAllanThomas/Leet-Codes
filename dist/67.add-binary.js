178120c6552e563fc1d8e031ab8c787049eda8f1
AdamAllanThomas <adamathomas100@gmail.com>
2023-06-04 16:31:11 -0600

    binary addition.

---
+++ dist/67.add-binary.js
@@ -0,0 +1,17 @@
+"use strict";
+function addBinary(a, b) {
+    // O(n)
+    let i = a.length - 1, j = b.length - 1, carry = 0, result = "";
+    while (i >= 0 || j >= 0) {
+        let sum = carry;
+        if (i >= 0)
+            sum += parseInt(a[i--], 10);
+        if (j >= 0)
+            sum += parseInt(b[j--], 10);
+        result = (sum % 2) + result;
+        carry = Math.floor(sum / 2);
+    }
+    if (carry !== 0)
+        result = carry + result;
+    return result;
+}
