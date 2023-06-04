3c62fd57210ee11667aa90e303bef0e2a5f7f6d1
AdamAllanThomas <adamathomas100@gmail.com>
2023-06-04 17:58:43 -0600

    Depth first search

---
+++ src/100.same-tree.ts
@@ -0,0 +1,22 @@
+`Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+Two binary trees are considered the same if they are structurally identical, and the nodes
+ have the same value.`;
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+  // O(n)
+  if (p === null && q === null) return true;
+  if (p === null || q === null) return false;
+  if (p.val !== q.val) return false;
+  else return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+}
+
+function isSameTreeOneLine(p: TreeNode | null, q: TreeNode | null): boolean {
+  // O(n)
+  return p === null && q === null
+    ? true
+    : p === null || q === null
+    ? false
+    : p.val !== q.val
+    ? false
+    : isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+}
