a23c2f91ecf1c58844d223bc269ac9c3a0237d1b
AdamAllanThomas <adamathomas100@gmail.com>
2023-06-04 19:40:38 -0600

    Sliding window susie

---
+++ dist/3.longest-substring-without-repeating-characters.js
@@ -0,0 +1,18 @@
+"use strict";
+`Given a string s, find the length of the longest substring without repeating characters.`;
+function lengthOfLongestSubstring(s) {
+    let letters = new Set();
+    let left = 0, right = 0, longest = 0;
+    while (right < s.length) {
+        if (!letters.has(s[right])) {
+            letters.add(s[right]);
+            longest = Math.max(longest, letters.size);
+            right++;
+        }
+        else {
+            letters.delete(s[left]);
+            left++;
+        }
+    }
+    return longest;
+}
