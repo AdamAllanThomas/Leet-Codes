86340d7994cc6d733bc2e0b66644ea20825c553e
AdamAllanThomas <adamathomas100@gmail.com>
2023-06-04 19:54:03 -0600

    test

--- dist/169.majority-element.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-`Given an array nums of size n, return the majority element.
-The majority element is the element that appears more than ⌊n / 2⌋ times.
- You may assume that the majority element always exists in the array.`;
-function majorityElement(nums) {
-    // O(n log n)
-    const sortedNums = nums.sort((a, b) => a - b);
-    return sortedNums[Math.floor(nums.length / 2)];
-}
